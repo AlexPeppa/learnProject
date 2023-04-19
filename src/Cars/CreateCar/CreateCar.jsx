1f6aacd4ef930468998bfc18ae846fdc984349db
Alex Yablokov <alex_peppa@mail.ru>
2023-04-19 07:26:26 +0300

    add addCar

---
+++ src/Cars/CreateCar/CreateCar.jsx
@@ -0,0 +1,73 @@
+import { Button, TextField } from "@mui/material"
+import { useState } from "react"
+import CarStyles from "./createCar.module.css";
+import { v4 as uuid4 } from 'uuid';
+import { Box } from "@mui/system";
+
+export const CreateCar = ({ setCarsState, setCarMode }) => {
+
+    const [newDataCar, setNewDataCar] = useState({
+        name: "",
+        description: {
+            text: "",
+            state: "",
+            models: [],
+            founded: '',
+        }
+    })
+    const [newCarInputModels, setNewCarInputModels] = useState('')
+
+    const newCar = {
+        [newDataCar.name.toLocaleUpperCase()]: {
+            ...newDataCar,
+            code: newDataCar.name.toLocaleUpperCase(),
+            id: uuid4(),
+        }
+    }
+
+    const addCar = () => {
+        setCarsState((prevState) => ({
+            ...prevState,
+            ...newCar,
+        })
+        )
+        setCarMode('READ')
+    }
+    const addModels = () => {
+        setNewDataCar((prevState) => ({
+            ...prevState,
+            description: {
+                ...prevState.description,
+                models: [...prevState.description.models, newCarInputModels]
+            }
+        }))
+        setNewCarInputModels('')
+    }
+
+    return (
+        <div>
+
+            <div className={CarStyles.addCarButtonDiv} > <Button className={CarStyles.addCarButton} onClick={addCar} > Add Car </Button></div>
+            <div className={CarStyles.dataCar}>
+                <div className={CarStyles.nameFoundState} ><div className={CarStyles.name} ><TextField required label='Car name' onChange={(event) => setNewDataCar((prevState) => ({ ...prevState, name: event.target.value }))} defaultValue='' /></div>
+                    <div className={CarStyles.founded}><TextField required label='Car founded' onChange={(event) => setNewDataCar((prevState) => ({ ...prevState, description: { ...prevState.description, founded: event.target.value } }))} defaultValue='' /></div>
+                    <div className={CarStyles.state}> <TextField required label='Car state' onChange={(event) => setNewDataCar((prevState) => ({ ...prevState, description: { ...prevState.description, state: event.target.value } }))} defaultValue='' /></div>  </div>
+                <div className={CarStyles.text}>
+                    <Box component="form" sx={{ '& .MuiTextField-root': { m: 1, width: '1100px' }, }}>
+                        <TextField onChange={(event) => setNewDataCar((prevState) => ({ ...prevState, description: { ...prevState.description, text: event.target.value } }))}
+                            id="outlined-multiline-static"
+                            label='Car text'
+                            multiline
+                            rows={4}
+                            defaultValue='' />
+                    </Box> </div>
+
+                <div><div className={CarStyles.models} ><TextField required label='Car models' onChange={(event) => setNewCarInputModels(event.target.value)} value={newCarInputModels} />
+                    <Button className={CarStyles.addModelButton} onClick={addModels}>Add Models</Button></div>
+                    <div className={CarStyles.liModels} >{newDataCar.description.models.map(model => <ul key={model} ><li>{model}</li></ul>)} </div>
+                </div>
+            </div>
+        </div>
+
+    )
+}
--- src/Cars/index.jsx
+++ src/Cars/index.jsx
@@ -2,6 +2,8 @@ import { useMemo, useState } from "react";
 import { Car } from "./Car/Car";
 import { cars } from "./constants";
 import styles from "./cars.module.css";
+import { Button } from "@mui/material";
+import { CreateCar } from "./CreateCar/CreateCar";
 
 export const Cars = () => {
   const [carMode, setCarMode] = useState('READ')
@@ -9,17 +11,23 @@ export const Cars = () => {
   const [selectedCar, setSelectedCar] = useState("BMW");
   const carsArr = useMemo(() => Object.values(carsState), [carsState])
 
+  const canCreate = carMode === 'CREATE';
 
+  const createCar = () => {
+    setCarMode('CREATE')
+  }
 
   return (
     <div styles={{ color: 'red' }} className={styles.wrapper}>
 
-      {carsArr.map((car) => (
-        <ul key={car.id}><li onClick={() => setSelectedCar(car.code)} className={`${styles.li} ${selectedCar === car.code ? styles.select : null}`}>{car.name}</li></ul>
-      ))}
-
-      <Car car ={carsState[selectedCar]}  setCarsState={setCarsState} carMode={carMode} setCarMode={setCarMode} />
-
+      {canCreate ? <div> <CreateCar setCarsState={setCarsState} setCarMode={setCarMode} /> </div> :
+        <div className={styles.carInfo} >
+          <div> <Button style={{ color: 'black', border: '1px solid black', marginLeft: '50px' }} onClick={createCar} variant="outlined" >Create car</Button>
+            {carsArr.map((car) => (
+              <ul key={car.id}><li onClick={() => setSelectedCar(car.code)} className={`${styles.li} ${selectedCar === car.code ? styles.select : null}`}>{car.name}</li></ul>
+            ))}</div>
+          <div className={styles.car} > <Car car={carsState[selectedCar]} setCarsState={setCarsState} carMode={carMode} setCarMode={setCarMode} /></div>
+        </div>}
     </div>
   );
 };
